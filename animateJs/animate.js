0d81a16d0c0c3b3a5359912c558f73b1ad9b7bb2
Simon <ssmdmsn@hotmail.com>
2016-08-20 20:30:01 +0800

    add some functions

--- animateJs/animate.js
+++ animateJs/animate.js
@@ -39,3 +39,172 @@ function getByClass(className, context, tag){
     return aResult;
 }
 
+/**
+ * 使用类似jQuery的$函数做为选择器的函数名
+ */
+/*function $(selector, context){
+    switch(selector.charAt(0)){
+        case '#':
+            return [id(selector.substring(1))];
+            break;
+        case '.':
+            return getByClass(selector.substring(1), context);
+            break;
+        default:
+            return tag(selector, context);
+    }
+}*/
+
+/**
+ * 修改或获取一个元素的属性
+ * @param elem 要修改的元素对象
+ * @param name 属性名
+ * @param value 属性值
+ */
+function attr(elem, name, value) {
+    // Make sure that a valid name was provided
+    if ( !name || name.constructor != String ) return '';
+    // Figure out if the name is one of the weird naming cases
+    name = { 'for': 'htmlFor', 'class': 'className' }[name] || name;
+    // If the user is setting a value, also
+    if ( value != null ) {
+        // Set the quick way first
+        elem[name] = value;
+        // If we can, use setAttribute
+        if ( elem.setAttribute )
+            elem.setAttribute(name,value);
+    }
+    // Return the value of the attribute
+    return elem[name] || elem.getAttribute(name) || '';
+}
+
+/**
+ * 修改或获取一个元素的value
+ * @param elem 要修改的元素对象
+ * @param value value属性的值
+ */
+function val(elem, value){
+    if(!value) return elem.value || "";
+
+    if(typeof value != "string" 
+        && typeof value != "number" 
+        && typeof value != "boolean")
+        return "";
+    elem.value = value;
+}
+
+/**
+ * 修改或获取一个元素的样式
+ * @param elem 要修改的元素对象
+ * @param name 属性名
+ * @param value 属性值
+ */
+function css(elem, attr, value){
+    if(!elem || typeof elem != "object") return;
+    if(arguments.length == 2){
+        if(typeof attr == "string"){
+            return _getStyle(elem, attr);//elem.style[attr];
+        }else if(typeof attr == "object"){
+            for(prop in attr){
+                setCss(prop, attr[prop]);
+                // elem.style[prop] = attr[prop];
+            }
+        }
+    }else if(arguments.length == 3){
+        setCss(attr, value);
+    }
+
+    function setCss(attr, value){
+        switch(attr){
+            case 'width':
+            case 'height':
+            case 'padding':
+            case 'paddingLeft':
+            case 'paddingRight':
+            case 'paddingTop':
+            case 'paddingBottom':
+                value = /\%/.test(value)?value:Math.max(parseInt(value), 0) + 'px';
+                break;
+            case 'left':
+            case 'top':
+            case 'bottom':
+            case 'right':
+            case 'margin':
+            case 'marginLeft':
+            case 'marginRight':
+            case 'marginTop':
+            case 'marginBottom':
+                value = /\%/.test(value)?value:parseInt(value) + 'px';
+                break;
+        }
+        elem.style[attr] = value;
+    }
+}
+
+function _getStyle( elem, name ) {//text-align, textAlign
+    // If the property exists in style[], 
+    //   then it’s been set recently (and is current)
+    //先将text-align => textAlign
+    name = name.replace(/-[a-z]/g, function(word){
+        return word.substring(1).toUpperCase();
+    });
+
+    if (elem.style[name])
+        return elem.style[name];
+
+    // Otherwise, try to use IE’s method
+    else if (elem.currentStyle)
+        return elem.currentStyle[name];
+
+    // Or the W3C’s method, if it exists
+    else if (document.defaultView && document.defaultView.getComputedStyle) {
+        // It uses the traditional ‘text-align’ style of rule writing, 
+        //   instead of textAlign
+        name = name.replace(/([A-Z])/g, "-$1");// textAlign =>  text-Align
+        name = name.toLowerCase(); //text-Align => text-align
+
+        // Get the style object and get the value of the property (if it exists)
+        var s = document.defaultView.getComputedStyle(elem,"");
+        return s && s.getPropertyValue(name);
+
+    // Otherwise, we’re using some other browser
+    } else
+        return null;
+}
+
+/**
+ * 为元素添加class
+ * @param elem 要修改的元素对象
+ * @param className 要添加的类名
+ */
+function addClass(elem, className){
+    if(!elem || typeof className != "string") return;
+
+    if(hasClass(elem, className)) return;
+
+    elem.className = elem.className + " " + className;
+}
+
+/**
+ * 判断元素是否包含指定的class
+ * @param elem 要查找的元素对象
+ * @param className 要判断的类名
+ */
+function hasClass(elem, className){
+    var re = new RegExp("\\b" + className + "\\b");
+    return re.test(elem.className);
+}
+
+/**
+ * 为元素移除class
+ * @param elem 要修改的元素对象
+ * @param className 要移除的类名
+ */
+function removeClass(elem, className){
+    if(!elem || typeof className != "string") return;
+
+    // if(!hasClass(elem, className)) return;
+    var re = new RegExp("\\b" + className + "\\b");
+    elem.className = trim(elem.className.replace(re, ""));
+}
+
